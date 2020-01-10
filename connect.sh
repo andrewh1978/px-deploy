b7164009509df3ca0bbbefc21c8cc804c88f7bd0
Andrew Hill <andrew@portworx.com>
2020-01-10 16:38:09 +0000

    check ip

--- connect.sh
+++ connect.sh
@@ -1,1 +1,6 @@
-ssh -oStrictHostKeyChecking=no root@$(aws ec2 describe-instances --region eu-west-1 --filters "Name=network-interface.vpc-id,Values=$AWS_vpc" "Name=tag:Name,Values=master-1" "Name=instance-state-name,Values=running" --query Reservations[*].Instances[*].PublicIpAddress --output text)
+ip=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=network-interface.vpc-id,Values=$AWS_vpc" "Name=tag:Name,Values=master-1" "Name=instance-state-name,Values=running" --query Reservations[*].Instances[*].PublicIpAddress --output text)
+if [ "$ip" ]; then
+  ssh -oStrictHostKeyChecking=no root@$ip
+else
+  echo Cannot get IP >&2
+fi
