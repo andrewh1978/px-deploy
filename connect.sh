da5c96f0985b24704c8b8efa2c556bcb7bb556f0
Andrew Hill <andrew@portworx.com>
2020-01-14 12:09:28 +0000

    use private ssh key

--- connect.sh
+++ connect.sh
@@ -1,6 +1,6 @@
 ip=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=network-interface.vpc-id,Values=$AWS_vpc" "Name=tag:Name,Values=master-1" "Name=instance-state-name,Values=running" --query Reservations[*].Instances[*].PublicIpAddress --output text)
 if [ "$ip" ]; then
-  ssh -oStrictHostKeyChecking=no root@$ip
+  ssh -oStrictHostKeyChecking=no -i id_rsa root@$ip
 else
   echo Cannot get IP >&2
 fi
