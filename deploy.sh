70859a31f8a9a01e1218062b2edf7581a9a5fdaf
Andrew Hill <andrew@portworx.com>
2020-01-24 17:00:20 +0000

    merge environment and deployment creation

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,13 @@
 #!/bin/bash
 
-DEP_PLATFORM=k8s
-DEP_CLUSTERS=1
-DEP_NODES=3
-DEP_K8S_VERSION=1.16.4
-DEP_PX_VERSION=2.3.2
-
-INIT_CLOUD=aws
-INIT_AWS_REGION=eu-west-1
-INIT_SSHKEY=$HOME/.ssh/id_rsa
-INIT_GCP_REGION=europe-west1
-INIT_GCP_ZONE=b
-
-AWS_TYPE=t3.large
-AWS_EBS="gp2:20"
-
-GCP_TYPE=n1-standard-2
-GCP_DISKS="pd-standard:20 pd-ssd:30"
+. ./defaults
 
 function envs_show() {
-  ( echo Environment,Cloud,Created
+  ( echo Environment,Cloud,Template,Created
   for e in $(find environments -type f); do
-    . $e
-    echo $(basename $e | cut -f 2- -d -),$INIT_CLOUD,$(perl -MPOSIX -e 'print strftime("%Y-%m-%d %H:%M:%S",gmtime((stat "'$e'")[10]))')
+    ( . $e
+    [[ ! "$DEP_TEMPLATE" ]] && DEP_TEMPLATE="<none>"
+    echo $(basename $e | cut -f 2- -d -),$DEP_CLOUD,$DEP_TEMPLATE,$(perl -MPOSIX -e 'print strftime("%Y-%m-%d %H:%M:%S",gmtime((stat "'$e'")[10]))') )
   done ) | column -t -s,
   exit
 }
@@ -31,13 +16,9 @@ function help_show() {
   cat <<EOF
 usage: $0 [ options ]
   -h				print this usage and exit
-  -d				debug - dump environmemnt
-  -n				dryrun - do not deploy or destroy
   --envs			list environments
   --env=name			specify environment in which to deploy
   --ssh				SSH to the first master node of the specified environment
-  --init=name			initialise a new AWS VPC or GCP project
-  --uninit=name			uninitialise (destroy) an AWS VPC or GCP project
   --destroy			destroy VMs
   --platform=k8s|ocp3		deploy Kubernetes or Openshift 3.11 (default $DEP_PLATFORM)
   --clusters=num		number of clusters to deploy (default $DEP_CLUSTERS)
@@ -50,19 +31,22 @@ usage: $0 [ options ]
   --gcp_disks="type: size..."	GCP disk volimes to be attached to each worker node (default "$GCP_DISKS")
   --template=name		name of template to deploy
 Additional flags for initialisation:
-  --region=name			specify AWS or GCP region (default $INIT_AWS_REGION or $INIT_GCP_REGION)
-  --cloud=aws|gcp		deploy on AWS or Google Cloud (default $INIT_CLOUD)
-  --sshkey=path			path to private SSH key associated with AWS IAM account or GCP project (default $INIT_SSHKEY)
-  --aws_keypair=name		name of AWS keypair
+  --region=name			specify AWS or GCP region (default $AWS_REGION or $GCP_REGION)
+  --cloud=aws|gcp		deploy on AWS or Google Cloud (default $DEP_CLOUD)
+  --sshkey=path			path to private SSH key associated with AWS IAM account or GCP project (default $DEP_SSHKEY)
+  --aws_keypair=name		name of AWS keypair (default $AWS_KEYPAIR)
 
 Examples:
-  Initialise on AWS:
-    $0 --init=abcBank-Demo --region=us-west-3 --aws_keypair=CHANGEME
+  Deploy single K8s cluster on AWS:
+    $0 --env=abcBank-Demo --region=us-west-3 --aws_keypair=myKeyPair
 
-  Uninitialise:
-    $0 --uninit=abcBank-Demo
+  Destroy the above example:
+    $0 --env=abcBank-Demo --destroy
 
-  Deploy a single K8s cluster:
+  Connect to the first master:
+    $0 --env=abcBank-Demo --ssh
+
+  Deploy a single K8s cluster with the defaults:
     $0 --env=abcBank-Demo
 
   Deploy a single clusterpair on Openshift and GCP:
@@ -75,57 +59,63 @@ EOF
 }
 
 function env_del_aws {
-  aws ec2 --region=$INIT_AWS_REGION delete-security-group --group-id $_AWS_sg &&
-  aws ec2 --region=$INIT_AWS_REGION delete-subnet --subnet-id $_AWS_subnet &&
-  aws ec2 --region=$INIT_AWS_REGION detach-internet-gateway --internet-gateway-id $_AWS_gw --vpc-id $_AWS_vpc &&
-  aws ec2 --region=$INIT_AWS_REGION delete-internet-gateway --internet-gateway-id $_AWS_gw &&
-  aws ec2 --region=$INIT_AWS_REGION delete-route-table --route-table-id $_AWS_routetable &&
-  aws ec2 --region=$INIT_AWS_REGION delete-vpc --vpc-id $_AWS_vpc &&
-  rm -f environments/$DEP_UNINIT
+  instances=$(aws ec2 describe-instances --region=$AWS_REGION --filters "Name=instance-state-name,Values=running" "Name=network-interface.vpc-id,Values=$_AWS_vpc" --query "Reservations[*].Instances[*].InstanceId" --output text)
+  [[ "$instances" ]] && {
+    aws ec2 terminate-instances --region $AWS_REGION --instance-ids $instances >/dev/null
+    aws ec2 wait instance-terminated --region $AWS_REGION --instance-ids $instances
+  }
+  aws ec2 --region=$AWS_REGION delete-security-group --group-id $_AWS_sg &&
+  aws ec2 --region=$AWS_REGION delete-subnet --subnet-id $_AWS_subnet &&
+  aws ec2 --region=$AWS_REGION detach-internet-gateway --internet-gateway-id $_AWS_gw --vpc-id $_AWS_vpc &&
+  aws ec2 --region=$AWS_REGION delete-internet-gateway --internet-gateway-id $_AWS_gw &&
+  aws ec2 --region=$AWS_REGION delete-route-table --route-table-id $_AWS_routetable &&
+  aws ec2 --region=$AWS_REGION delete-vpc --vpc-id $_AWS_vpc &&
+  rm -f environments/$DEP_ENV
 }
 
 function env_del_gcp {
-  gcloud projects delete $_GCP_project --quiet && rm -f environments/$DEP_UNINIT
+  gcloud projects delete $_GCP_project --quiet && rm -f environments/$DEP_ENV px-deploy_gcp_$_GCP_project.json
 }
 
 function env_create_aws {
-  _AWS_vpc=$(aws --region=$INIT_AWS_REGION --output text ec2 create-vpc --cidr-block 192.168.0.0/16 --query Vpc.VpcId)
-  _AWS_subnet=$(aws --region=$INIT_AWS_REGION --output text ec2 create-subnet --vpc-id $_AWS_vpc --cidr-block 192.168.0.0/16 --query Subnet.SubnetId)
-  _AWS_gw=$(aws --region=$INIT_AWS_REGION --output text ec2 create-internet-gateway --query InternetGateway.InternetGatewayId)
-  aws --region=$INIT_AWS_REGION ec2 attach-internet-gateway --vpc-id $_AWS_vpc --internet-gateway-id $_AWS_gw
-  _AWS_routetable=$(aws --region=$INIT_AWS_REGION --output text ec2 create-route-table --vpc-id $_AWS_vpc --query RouteTable.RouteTableId)
-  aws --region=$INIT_AWS_REGION ec2 create-route --route-table-id $_AWS_routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $_AWS_gw >/dev/null
-  aws --region=$INIT_AWS_REGION ec2 associate-route-table  --subnet-id $_AWS_subnet --route-table-id $_AWS_routetable >/dev/null
-  _AWS_sg=$(aws --region=$INIT_AWS_REGION --output text ec2 create-security-group --group-name px-cloud --description "Security group for px-cloud" --vpc-id $_AWS_vpc --query GroupId)
-  aws --region=$INIT_AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 22 --cidr 0.0.0.0/0
-  aws --region=$INIT_AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 443 --cidr 0.0.0.0/0
-  aws --region=$INIT_AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 8080 --cidr 0.0.0.0/0
-  aws --region=$INIT_AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0
-  aws --region=$INIT_AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol all --cidr 192.168.0.0/16
-  aws --region=$INIT_AWS_REGION ec2 create-tags --resources $_AWS_vpc $_AWS_subnet $_AWS_gw $_AWS_routetable $_AWS_sg --tags Key=px-deploy_name,Value=$DEP_INIT
-  _AWS_ami=$(aws --region=$INIT_AWS_REGION --output text ec2 describe-images --owners 679593333241 --filters Name=name,Values='CentOS Linux 7 x86_64 HVM EBS*' Name=architecture,Values=x86_64 Name=root-device-type,Values=ebs --query 'sort_by(Images, &Name)[-1].ImageId')
-    set | grep -E '^(INIT_|_AWS)' | grep -v GCP >environments/$DEP_INIT
+  _AWS_vpc=$(aws --region=$AWS_REGION --output text ec2 create-vpc --cidr-block 192.168.0.0/16 --query Vpc.VpcId)
+  _AWS_subnet=$(aws --region=$AWS_REGION --output text ec2 create-subnet --vpc-id $_AWS_vpc --cidr-block 192.168.0.0/16 --query Subnet.SubnetId)
+  _AWS_gw=$(aws --region=$AWS_REGION --output text ec2 create-internet-gateway --query InternetGateway.InternetGatewayId)
+  aws --region=$AWS_REGION ec2 attach-internet-gateway --vpc-id $_AWS_vpc --internet-gateway-id $_AWS_gw
+  _AWS_routetable=$(aws --region=$AWS_REGION --output text ec2 create-route-table --vpc-id $_AWS_vpc --query RouteTable.RouteTableId)
+  aws --region=$AWS_REGION ec2 create-route --route-table-id $_AWS_routetable --destination-cidr-block 0.0.0.0/0 --gateway-id $_AWS_gw >/dev/null
+  aws --region=$AWS_REGION ec2 associate-route-table  --subnet-id $_AWS_subnet --route-table-id $_AWS_routetable >/dev/null
+  _AWS_sg=$(aws --region=$AWS_REGION --output text ec2 create-security-group --group-name px-cloud --description "Security group for px-cloud" --vpc-id $_AWS_vpc --query GroupId)
+  aws --region=$AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 22 --cidr 0.0.0.0/0 &
+  aws --region=$AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 443 --cidr 0.0.0.0/0 &
+  aws --region=$AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 8080 --cidr 0.0.0.0/0 &
+  aws --region=$AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol tcp --port 30000-32767 --cidr 0.0.0.0/0 &
+  aws --region=$AWS_REGION ec2 authorize-security-group-ingress --group-id $_AWS_sg --protocol all --cidr 192.168.0.0/16 &
+  aws --region=$AWS_REGION ec2 create-tags --resources $_AWS_vpc $_AWS_subnet $_AWS_gw $_AWS_routetable $_AWS_sg --tags Key=px-deploy_name,Value=$DEP_ENV &
+  _AWS_ami=$(aws --region=$AWS_REGION --output text ec2 describe-images --owners 679593333241 --filters Name=name,Values='CentOS Linux 7 x86_64 HVM EBS*' Name=architecture,Values=x86_64 Name=root-device-type,Values=ebs --query 'sort_by(Images, &Name)[-1].ImageId')
+  wait
+  set | grep -E '^(DEP|AWS|_AWS)' | grep -v GCP >environments/$DEP_ENV
 }
 
 function env_create_gcp {
   _GCP_project=pxd-$(uuidgen | tr -d -- - | cut -b 1-26 | tr 'A-Z' 'a-z')
-  gcloud projects create $_GCP_project --labels px-deploy_name=$DEP_INIT
+  gcloud projects create $_GCP_project --labels px-deploy_name=$DEP_ENV
   account=$(gcloud alpha billing accounts list | tail -1 | cut -f 1 -d " ")
   gcloud alpha billing projects link $_GCP_project --billing-account $account
   gcloud services enable compute.googleapis.com --project $_GCP_project
   gcloud compute networks create px-net --project $_GCP_project
-  gcloud compute networks subnets create --range 192.168.0.0/16 --network px-net px-subnet --region $INIT_GCP_REGION --project $_GCP_project
+  gcloud compute networks subnets create --range 192.168.0.0/16 --network px-net px-subnet --region $GCP_REGION --project $_GCP_project
   gcloud compute firewall-rules create allow-internal --allow=tcp,udp,icmp --source-ranges=192.168.0.0/16 --network px-net --project $_GCP_project &
   gcloud compute firewall-rules create allow-external --allow=tcp:22,tcp:443,tcp:6443 --network px-net --project $_GCP_project &
-  gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat $INIT_SSHKEY.pub)" --project $_GCP_project &
+  gcloud compute project-info add-metadata --metadata "ssh-keys=$USER:$(cat $DEP_SSHKEY.pub)" --project $_GCP_project &
   service_account=$(gcloud iam service-accounts list --project $_GCP_project --format 'flattened(email)' | tail -1 | cut -f 2 -d " ")
   _GCP_key=$(gcloud iam service-accounts keys create /dev/stdout --iam-account $service_account | base64)
   wait
-  set | grep -E '^(INIT_|_GCP)' | grep -v AWS >environments/$DEP_INIT
+  set | grep -E '^(DEP|GCP|_GCP)' | grep -v AWS >environments/$DEP_ENV
 }
 
 
-options=$(getopt -o dnh --long envs,env:,ssh,init:,uninit:,region:,aws_keypair:,sshkey:,platform:,cloud:,clusters:,nodes:,k8s_version:,px_version:,aws_type:,aws_ebs:,gcp_type:,gcp_disks,gcp_zone:,template:,destroy -- "$@")
+options=$(getopt -o dnh --long envs,env:,ssh,region:,aws_keypair:,sshkey:,platform:,cloud:,clusters:,nodes:,k8s_version:,px_version:,aws_type:,aws_ebs:,gcp_type:,gcp_disks,gcp_zone:,template:,destroy -- "$@")
 [ $? -eq 0 ] || { 
   echo "Incorrect options provided"
   exit 1
@@ -158,12 +148,6 @@ done
 eval set -- "$options"
 while true; do
   case "$1" in
-  -d)
-    DEP_DEBUG=1
-    ;;
-  -n)
-    DEP_DRYRUN=1
-    ;;
   --envs)
     DEP_ENVS=1
     ;;
@@ -181,43 +165,27 @@ while true; do
   --ssh)
     DEP_SSH=1
     ;;
-  --init)
-    shift;
-    DEP_INIT=$1
-    [[ ! $DEP_INIT =~ ^[a-zA-Z0-9_\-]+$ ]] && {
-      echo "Bad initialisation environment name"
-      exit 1
-    }
-    ;;
-  --uninit)
-    shift;
-    DEP_UNINIT=$1
-    [[ ! $DEP_UNINIT =~ ^[a-zA-Z0-9_\-]+$ ]] && {
-      echo "Bad uninitialisation environment name"
-      exit 1
-    }
-    ;;
   --region)
     shift;
-    INIT_AWS_REGION=$1
-    INIT_GCP_REGION=$1
-    [[ ! "$INIT_AWS_REGION" =~ ^[a-zA-Z0-9_\-]+$ ]] && {
+    AWS_REGION=$1
+    GCP_REGION=$1
+    [[ ! "$AWS_REGION" =~ ^[a-zA-Z0-9_\-]+$ ]] && {
       echo "Bad region"
       exit 1
     }
     ;;
   --aws_keypair)
     shift;
-    INIT_AWS_KEYPAIR=$1
-    [[ ! "$INIT_AWS_KEYPAIR" =~ ^[a-zA-Z0-9_\-]+$ ]] && {
+    AWS_KEYPAIR=$1
+    [[ ! "$AWS_KEYPAIR" =~ ^[a-zA-Z0-9_\-]+$ ]] && {
       echo "Bad keypair name"
       exit 1
     }
     ;;
   --sshkey)
     shift;
-    INIT_SSHKEY=$1
-    [[ ! -f "$INIT_SSHKEY" ]] && {
+    DEP_SSHKEY=$1
+    [[ ! -f "$DEP_SSHKEY" ]] && {
       echo "Bad SSH key"
       exit 1
     }
@@ -232,8 +200,8 @@ while true; do
     ;;
   --cloud)
     shift;
-    INIT_CLOUD=$1
-    [[ ! $INIT_CLOUD =~ ^aws|gcp$ ]] && {
+    DEP_CLOUD=$1
+    [[ ! $DEP_CLOUD =~ ^aws|gcp$ ]] && {
       echo "Bad cloud"
       exit 1
     }
@@ -320,39 +288,35 @@ done
 
 [[ ! -d environments ]] && mkdir environments
 [[ "$DEP_HELP" ]] && help_show
-[[ "$DEP_DEBUG" ]] && set | grep -E '^(DEP|AWS|GCP|INIT)' | sort
-[[ "$DEP_DRYRUN" ]] && exit
 [[ "$DEP_ENVS" ]] && envs_show
 
-[[ "$DEP_UNINIT" ]] && {
-  . environments/$DEP_UNINIT
-  [[ "$INIT_CLOUD" == aws ]] && env_del_aws
-  [[ "$INIT_CLOUD" == gcp ]] && env_del_gcp
-  exit
-}
-
-[[ "$DEP_INIT" ]] && {
-  [[ "$DEP_DEBUG" ]] && set | grep ^INIT | sort
-  [[ "$INIT_CLOUD" == aws ]] && [[ ! "$INIT_AWS_KEYPAIR" ]] && echo Must set AWS keypair && exit
-  [[ "$INIT_CLOUD" == gcp ]] && [[ ! "$GCP_KEYFILE" ]]
-  env_create_$INIT_CLOUD
-  exit
-}
-
-[[ ! "$DEP_ENV" ]] && echo Must specify environment && exit
-. environments/$DEP_ENV
-export $(set | grep -E '^(DEP|AWS|GCP|INIT|_AWS|_GCP)' | cut -f 1 -d = )
-
 [[ "$DEP_SSH" ]] && {
-  [[ "$INIT_CLOUD" == aws ]] && ip=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=network-interface.vpc-id,Values=$_AWS_vpc" "Name=tag:Name,Values=master-1" "Name=instance-state-name,Values=running" --query Reservations[*].Instances[*].PublicIpAddress --output text)
-  [[ "$INIT_CLOUD" == gcp ]] && ip=$(gcloud compute instances list --project $_GCP_project --filter="name=('master-1')" --format 'flattened(networkInterfaces[0].accessConfigs[0].natIP)' | tail -1 | cut -f 2 -d " ")
+  [[ ! "$DEP_ENV" ]] && echo Must specify --env for SSH && exit
+  [[ ! -f "environments/$DEP_ENV" ]] && echo Environment $DEP_ENV does not exist && exit
+  . environments/$DEP_ENV
+  export $(set | grep -E '^(DEP|AWS|GCP|_AWS|_GCP)' | cut -f 1 -d = )
+  [[ "$DEP_CLOUD" == aws ]] && ip=$(aws ec2 describe-instances --region $AWS_REGION --filters "Name=network-interface.vpc-id,Values=$_AWS_vpc" "Name=tag:Name,Values=master-1" "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+  [[ "$DEP_CLOUD" == gcp ]] && ip=$(gcloud compute instances list --project $_GCP_project --filter="name=('master-1')" --format 'flattened(networkInterfaces[0].accessConfigs[0].natIP)' | tail -1 | cut -f 2 -d " ")
   ssh -oStrictHostKeyChecking=no -i id_rsa root@$ip
   exit
 }
 
-
 if [ "$DEP_DESTROY" ]; then
-  vagrant destroy -fp
+  [[ ! "$DEP_ENV" ]] && echo Must specify --env for destroy && exit
+  [[ ! -f "environments/$DEP_ENV" ]] && echo Environment $DEP_ENV does not exist && exit
+  . environments/$DEP_ENV
+  #export $(set | grep -E '^(DEP|AWS|GCP|_AWS|_GCP)' | cut -f 1 -d = )
+  env_del_$DEP_CLOUD
 else
+  if [[ "$DEP_ENV" ]]; then
+    [[ -e "environments/$DEP_ENV" ]] && echo Environment $DEP_ENV already exists && exit
+  else
+    DEP_ENV=$(uuidgen)
+  fi
+  #export DEP_ENV
+  env_create_$DEP_CLOUD
+  #. environments/$DEP_ENV
+  export $(set | grep -E '^(DEP|AWS|GCP|_AWS|_GCP)' | cut -f 1 -d = )
   vagrant up
+  rm -rf .vagrant
 fi
