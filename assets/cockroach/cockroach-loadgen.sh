9dacaa384083e0a032b08aca8a6695db5734f29b
Andrew Hill <andrewh1978@users.noreply.github.com>
2020-04-01 10:32:08 +0100

    typo

--- assets/cockroach/cockroach-loadgen.sh
+++ assets/cockroach/cockroach-loadgen.sh
@@ -10,6 +10,6 @@ until kubectl exec cockroachdb-0 -- ./cockroach workload init tpcc --warehouses=
   # "Error: failed insert into customer: pq: result is ambiguous (error=rpc error: code = Unavailable desc = transport is closing"
   # which is related to saturation and there is no max configurations for payload or op rate. 
   # so we simply retry.
-  echo Tansfer disrupted, retrying...
+  echo Transfer disrupted, retrying...
   sleep 1
 done
