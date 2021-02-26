16dafa1bb0e04dd8473ec1e8add353d64609b39b
Andrew Hill <andrew@portworx.com>
2021-02-26 16:22:36 +0000

    clean up px volumes on eks

--- px-deploy.go
+++ px-deploy.go
@@ -580,13 +580,13 @@ func destroy_deployment(name string) {
 				    vpc=$(eksctl utils describe-stacks --region ` + config.Aws_Region + ` --cluster px-deploy-` + config.Name + `-$(hostname | cut -f 2 -d -) | grep vpc- | cut -f 2 -d \")
 				    profile=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$vpc" --region ` + config.Aws_Region + ` --query Reservations[0].Instances[0].IamInstanceProfile.Arn --output text | cut -f 2 -d /)
 				    instances=$(aws ec2 describe-instances --filters "Name=vpc-id,Values=$vpc" --region ` + config.Aws_Region + ` --query Reservations[].Instances[].InstanceId --output text)
-				    #volumes=$(for j in $instances; do aws ec2 describe-volumes --region ` + config.Aws_Region + ` --filters "Name=attachment.instance-id,Values=$j" "Name=tag:PWX_CLUSTER_ID,Values=$PX_CLUSTER_NAME" --query Volumes[].Attachments[].VolumeId --output text; done)
+				    volumes=$(for j in $instances; do aws ec2 describe-volumes --region ` + config.Aws_Region + ` --filters "Name=attachment.instance-id,Values=$j" "Name=tag:PWX_CLUSTER_ID,Values=px-deploy-$(hostname | cut -f 2 -d -)" --query Volumes[].Attachments[].VolumeId --output text; done)
 				    role=$(aws iam get-instance-profile --instance-profile-name $profile --region ` + config.Aws_Region + ` --query InstanceProfile.Roles[0].RoleName --output text)
 				    aws iam delete-role-policy --role-name $role --policy-name px-eks-policy --region ` + config.Aws_Region + `
 				    eksctl delete cluster --region ` + config.Aws_Region + ` --name px-deploy-` + config.Name + `-$(hostname | cut -f 2 -d -) --wait >&/tmp/delete
-				    #for j in $volumes; do
-				      #aws ec2 delete-volume --region ` + config.Aws_Region + ` --volume-id $j
-				    #done
+				    for j in $volumes; do
+				      aws ec2 delete-volume --region ` + config.Aws_Region + ` --volume-id $j
+				    done
 EOF
 				done
 			`).Run()
