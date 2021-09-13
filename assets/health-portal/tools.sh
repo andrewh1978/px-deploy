d6d73f05add9f9d897e5120ef710737542f7033a
Kai Davenport <kaiyadavenport@gmail.com>
2021-09-13 16:05:37 +0100

    install backups for health-portal

--- assets/health-portal/tools.sh
+++ assets/health-portal/tools.sh
@@ -196,6 +196,15 @@ function install_autopilot() {
   kubectl apply -f /assets/monitoring/prometheus-rules.yaml
 }
 
+function install_backups() {
+  echo "installing backups"
+  kubectl apply -f /assets/minio/minio-deployment.yml
+  ip=`curl -s https://ipinfo.io/ip`
+  sed -i -e 's/xxxx/'"$ip"'/g' /assets/backup-restore/backupLocation.yml
+  kubectl wait --for=condition=ready pod -l app=minio -n minio --timeout 30m
+  docker run --rm -v /etc/hosts:/etc/hosts -e AWS_ACCESS_KEY_ID=minio -e AWS_SECRET_ACCESS_KEY=minio123 amazon/aws-cli --endpoint-url http://node-$cluster-1:30221 s3 mb s3://portworx
+}
+
 function url_summary() {
   echo ""
   echo "-------------------------------------------------------"
