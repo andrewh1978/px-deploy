663cbc2aace4b60ddc05fba7f29e4323774e83a2
Andrew Hill <ahill@purestorage.com>
2023-03-24 17:25:02 +0000

    training fixes

--- assets/training/install-pxc.sh
+++ assets/training/install-pxc.sh
@@ -1,6 +1,7 @@
 mkdir -p $HOME/bin
 curl -Ls https://github.com/portworx/pxc/releases/download/v0.33.0/pxc-v0.33.0.linux.amd64.tar.gz | tar Oxzf - pxc/kubectl-pxc | tee $HOME/bin/kubectl-pxc >/dev/null
 curl -so $HOME/bin/pxc-pxctl https://raw.githubusercontent.com/portworx/pxc/master/component/pxctl/pxc-pxctl
-chmod +x $HOME/bin/pxc-pxctl $HOME/bin/kubectl-pxc
+kubectl cp -n kube-system $(kubectl get pod -n kube-system -l name=stork -o jsonpath='{.items[0].metadata.name}'):/storkctl/linux/storkctl $HOME/bin/storkctl
+chmod +x $HOME/bin/pxc-pxctl $HOME/bin/kubectl-pxc $HOME/bin/storkctl
 echo "alias pxctl='kubectl pxc pxctl'" >>$HOME/.bashrc
 echo "alias watch='watch --color '" >>$HOME/.bashrc
